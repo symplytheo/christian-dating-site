729a5513bb0afe0fe0b9ce4f0f97fecc60548d08
Theo Iyonor <symplytheo@gmail.com>
2021-11-16 18:40:59 +0100

    project theme and lint setup

---
+++ plugins/vuetify.config.js
@@ -0,0 +1,21 @@
+const theme = {
+  dark: false,
+  themes: {
+    light: {
+      primary: '#FF47D3',
+    },
+  },
+}
+
+const breakpoint = {
+  mobileBreakpoint: 'sm',
+  thresholds: {
+    xs: 480,
+    sm: 768,
+    md: 1024,
+    lg: 1200,
+  },
+  scrollBarWidth: 16,
+}
+
+export default { theme, breakpoint }
