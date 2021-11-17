54f00d3d5266602b860a292a7b51e81c040dfd86
Theo Iyonor <symplytheo@gmail.com>
2021-11-17 10:32:52 +0100

    Make vuetify image placeholder a component

---
+++ components/ImagePlaceholder.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row class="fill-height" justify="center" align="center">
+    <v-progress-circular
+      :color="color"
+      :indeterminate="indeterminate"
+      v-bind="$attrs"
+    />
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    color: { type: String, default: 'secondary' },
+    indeterminate: { type: Boolean, default: true },
+  },
+}
+</script>
