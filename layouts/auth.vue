9ce16c4308381bb6119d59e8330e49f0ef759dbe
Theo Iyonor <symplytheo@gmail.com>
2021-11-17 10:46:18 +0100

    Add layout for auth pages

---
+++ layouts/auth.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app class="bg">
+    <main>
+      <nuxt />
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.bg {
+  background-image: linear-gradient(
+      rgba(233, 22, 118, 0.3),
+      rgba(233, 22, 118, 0.3)
+    ),
+    url(/bg/1.jpg);
+  background-blend-mode: 'multiply';
+  background-size: 'cover';
+  background-position: center;
+  background-repeat: no-repeat;
+}
+</style>
