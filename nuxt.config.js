42a7748139f76f49bfbe2131636c9ad21bfeb528
Theo Iyonor <symplytheo@gmail.com>
2021-11-17 23:30:30 +0100

    set target to static

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,10 @@
 export default {
+    // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - Christian Dating Site',
