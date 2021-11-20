7c33caed70948c72f30d6d0031bda94ae23ad99e
Theo Iyonor <symplytheo@gmail.com>
2021-11-20 08:22:53 +0100

    update content

---
+++ pages/about-us.vue
@@ -0,0 +1,255 @@
+<template>
+  <v-container class="rounded my-10">
+    <v-card flat class="pa-5">
+      <h2 class="text-h5 text-md-h4 font-weight-black mb-5 text-center">
+        About Us
+      </h2>
+
+      <div id="who-we-are">
+        <h4 class="text-h6 text-md-h5 font-weight-bold mb-2 text-center">
+          Who We Are
+        </h4>
+        <p>
+          Christian Dating and Social Networking was the vision of Ebere Nneji.
+          She realised that it has become increasingly difficult to meet people
+          who are genuine Christians and serious about their faith, in an ever
+          more secular world, and this is even more challenging on online
+          platforms be it for dating, socials or other purposes. Her passion for
+          seeing people form God-centered relationships and to see Christian
+          marriages form and blossom led to the development of Christian Dating
+          and Social Networking. She also wanted it to be more than just a
+          dating site, but to be a place of community.
+        </p>
+        <p>
+          Right from the days of Adam and Eve, to the days of our grandparents
+          and parents, and to present day, life has changed a lot; and so has
+          the way we meet people, including that special someone. The internet
+          has increasingly become the go-to-place for almost all of life’s needs
+          including finding love. It is therefore no surprise that online dating
+          websites are all over the internet nowadays and more and more are
+          springing up day by day. As the scene becomes more proliferated so are
+          the security failures involving fraudulent behaviours and scams that
+          have affected a lot of innocent users of online dating websites. There
+          are now so many bogus sites which consist of a deluge of fake
+          profiles, non-Christians and scammers.
+        </p>
+        <p>
+          Christian Dating and Social Networking is an online platform that is
+          built to provide a safe place for genuine Christians to meet other
+          like-minded Christian singles for real meaningful relationships,
+          potentially leading to marriage. At Christian Dating and Social
+          Networking, safety is at the heart of everything we do. We take fake
+          profiles and scammers seriously, making us one of the safest dating
+          websites of our time. We do not buy profiles from other websites. All
+          profiles on our website are screened by us prior to publishing.
+        </p>
+        <p>
+          Our mission is to also empower, inspire and encourage single
+          Christians in their faith and day-to-day lives whether or not they are
+          actively looking for a partner. We have very active and lively
+          discussion boards and a system that allows members to organise local
+          meetup activities. We also organise offline corporate events to give
+          people a chance to meet face to face in a safe and comfortable
+          environment. Our corporate events cover a range of themes including
+          seminars, conferences, retreats, get-aways, music festivals, films,
+          theatres, comedy nights etc.
+        </p>
+        <p>
+          Whilst we are not a match-making or marriage advice agency, we hope we
+          have provided a platform that will empower you to take the right steps
+          in order to find that special someone and/or improve yourself with our
+          range of meetups and events whilst you are searching.
+        </p>
+      </div>
+
+      <div id="what-we-believe" class="mt-10">
+        <h4 class="text-h6 text-md-h5 font-weight-bold mb-2 text-center">
+          What We Believe
+        </h4>
+        <div>
+          <ul>
+            <li>
+              We believe in the Holy Trinity (God the Father, God the Son and
+              God the Holy Spirit).
+            </li>
+            <li>
+              We believe the Bible as the true and only authentic Word of God.
+            </li>
+            <li>
+              We believe the Biblical canon as the sole authoritative scripture
+            </li>
+            <li>
+              We believe that Jesus Christ is the Son of God and He was
+              crucified, dead, buried, resurrected and ascended into heaven
+            </li>
+            <li>
+              We believe that salvation comes by accepting the sacrifice of
+              Jesus Christ and acknowledging Him as your personal Lord and
+              Saviour
+            </li>
+            <li>We believe strongly in the sanctity of marriage</li>
+          </ul>
+        </div>
+      </div>
+
+      <div id="core-values" class="mt-10">
+        <h4 class="text-h6 text-md-h5 font-weight-bold mb-2 text-center">
+          Our Core Values
+        </h4>
+        <p>
+          We are in this together. Our core values are the deeply engrained
+          principles that guide all of our actions in delivering the Service to
+          our members. They also represent the set of behaviours we expect from
+          all our members who use our Service. These are:
+        </p>
+        <div>
+          <h6 class="text-subtitle-1 text-sm-h6 font-weight-bold">Safety</h6>
+          <p>
+            The safety of our members is our highest priority. We are committed
+            to the highest level of safety and security on our platform and at
+            our events. Members are expected to exhibit behaviours that will
+            make other members feel safe and not threatened in any way while
+            using our platform or attending our meetups and events. You can read
+            our <nuxt-link to="/safety-advice">Safety Tips</nuxt-link>.
+          </p>
+        </div>
+        <div>
+          <h6 class="text-subtitle-1 text-sm-h6 font-weight-bold">Trust</h6>
+          <p>
+            We want our members to be able to trust us with their information.
+            We will take all steps reasonably necessary to ensure that your data
+            is treated securely and in accordance with our
+            <nuxt-link to="/legal#privacy-policy">Privacy Policy</nuxt-link>. We
+            expect our members to protect any personal information they may
+            obtain about another member in the course of using our Website, our
+            Service or communicating with that member.
+          </p>
+        </div>
+        <div>
+          <h6 class="text-subtitle-1 text-sm-h6 font-weight-bold">Respect</h6>
+          <p>
+            We recognise that the Christian faith and traditions are broad; we
+            welcome Christians from a range of faith traditions and church
+            denominations; and we respect each member’s beliefs and values. We
+            only ask that you join us only if you believe and accept
+            <nuxt-link to="#what-we-believe">What We Believe</nuxt-link>. By
+            joining Christian Dating and Social Networking, you are committing
+            to treating other members with respect, and not to attack or
+            undermine the faith, beliefs or traditions of any member, whether
+            directly or indirectly, whether online or offline. See our
+            <nuxt-link to="/legal#terms-and-conditions"
+              >Terms and Conditions</nuxt-link
+            >.
+          </p>
+        </div>
+        <div>
+          <h6 class="text-subtitle-1 text-sm-h6 font-weight-bold">Honesty</h6>
+          <p>
+            We will not make false promises. We will be honest about our
+            strengths and weaknesses, what we can or cannot deliver. We expect
+            our members to be honest about who they are and not claim to be what
+            they are not. Your profile gives you the opportunity to give honest
+            information about yourself. We hope that by signing up to Christian
+            Dating and Social Networking means that your Christian faith is
+            important to you.
+          </p>
+        </div>
+        <div>
+          <h6 class="text-subtitle-1 text-sm-h6 font-weight-bold">Quality</h6>
+          <p>
+            What sets us apart is our ability to help our members make quality
+            connections. We feature robust profiles and personalisation features
+            to help members connect with genuine people. Our suite of powerful
+            communication tools helps members make deeper and more personal
+            connections that should potentially lead to a lasting relationship.
+            We encourage members to work at each other’s pace, keep
+            communication online and only meet offline when both parties are
+            absolutely comfortable and feel safe to do so. No member should put
+            undue pressure on another.
+          </p>
+        </div>
+        <div>
+          <h6 class="text-subtitle-1 text-sm-h6 font-weight-bold">
+            Empowerment
+          </h6>
+          <p>
+            Whether you are actively looking for a partner or not, it is our
+            desire at Christian Dating and Social Networking to see all single
+            Christians empowered, inspired and encouraged in their faith and
+            day-to-day lives. We have very active and lively discussion boards
+            and a system which allows members to organise local meetups. We also
+            offer offline events to give people a chance to meet face to face in
+            a safe and comfortable environment.
+          </p>
+        </div>
+      </div>
+
+      <div id="why-join-us" class="mt-10">
+        <h4 class="text-h6 text-md-h5 font-weight-bold mb-2 text-center">
+          Why You Should Join Us
+        </h4>
+        <div class="mb-3">
+          <ul>
+            <li>
+              We manually approve and monitor ALL profiles, so it's your safest
+              place to meet single Christians online.
+            </li>
+            <li>
+              We do not share profiles with non-Christian companies/sites.
+            </li>
+            <li>
+              There is no charge for our Trial Membership which gives you access
+              to ALL features.
+            </li>
+            <li>You can cancel at anytime!</li>
+            <li>
+              Membership increases your chances and opportunities to find love
+              and relationships outside of your own church, workplace, social
+              circle etc.
+            </li>
+            <li>
+              Our various meetups and corporate events will improve your
+              personal wellbeing and help develop you as an individual.
+            </li>
+            <li>We will always aim to deliver excellent customer service.</li>
+          </ul>
+        </div>
+        <p>
+          It is our fervent prayer that our Website helps on your journey to
+          finding and meeting genuine single Christians, and ultimately to find
+          real and lasting love and happiness in marriage.
+        </p>
+        <p>
+          It's free to join, and there is no cost for a trial membership. You
+          can take a look at our
+          <nuxt-link to="">membership types</nuxt-link>.
+        </p>
+        <p>
+          What are you still waiting for?
+          <nuxt-link to="/">Sign up</nuxt-link> to start connecting immediately
+          with likeminded real single Christians.
+        </p>
+        <p>God bless you!</p>
+      </div>
+
+      <div id="more-info" class="mt-10">
+        <h4 class="text-h6 text-md-h5 font-weight-bold mb-2 text-center">
+          Need More Info?
+        </h4>
+        <p>
+          If you have any other questions which aren't covered here, take a look
+          at our <nuxt-link to="/faqs">Frequently Asked Questions</nuxt-link> or
+          you can <nuxt-link to="/contact-us">contact us</nuxt-link>.
+        </p>
+      </div>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  head() {
+    return { title: 'About Us' }
+  },
+}
+</script>
