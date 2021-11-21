43a1109efeb3b366aa0516ea98a289b4e5a4ce42
Theo Iyonor <symplytheo@gmail.com>
2021-11-21 19:03:27 +0100

    create page

---
+++ pages/love-pod/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container class="my-10">
+    <v-card flat tile class="pa-10 primary--text">Love Pod</v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  layout: 'secondary',
+}
+</script>
+
+<style></style>
