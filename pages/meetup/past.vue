7d411db15101e9fff815cb6e7dfd5823beb63222
Theo Iyonor <symplytheo@gmail.com>
2021-11-21 19:00:16 +0100

    create child pages for meetup

---
+++ pages/meetup/past.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-row>
+      <v-col v-for="(item, n) in meetups" :key="n" cols="12" sm="6" lg="4">
+        <cards-meetup no-action :meetup="item" />
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="12" sm="8" md="6" lg="4" class="text-center">
+        <v-btn block large color="primary" depressed>See More</v-btn>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return { title: 'Past | Meetups' }
+  },
+  computed: {
+    meetups() {
+      const arr = this.$store.state.meetups
+      return [...arr, ...arr, ...arr]
+    },
+  },
+}
+</script>
+
+<style></style>
