ba5fba2723af4e20acbcb5ccf435486da12a24c6
Theo Iyonor <symplytheo@gmail.com>
2021-11-21 19:01:56 +0100

    create secondary layout

---
+++ layouts/secondary.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <core-app-bar extended :tabs="tabs" :items="loggedNav" />
+
+    <v-main class="grey lighten-3">
+      <v-container fluid class="pa-0 ma-0">
+        <nuxt />
+      </v-container>
+    </v-main>
+
+    <core-footer />
+  </v-app>
+</template>
+
+<script>
+export default {
+  computed: {
+    tabs() {
+      return this.$store.state.tabs
+    },
+    isMobile() {
+      return this.$vuetify.breakpoint.mobile
+    },
+    userMenu() {
+      return this.$store.state.user.userMenu
+    },
+    loggedNav() {
+      return this.$store.state.loggedNav
+    },
+  },
+  mounted() {
+    this.$store.commit('user/setUser', { name: 'Jane Doe' })
+  },
+}
+</script>
