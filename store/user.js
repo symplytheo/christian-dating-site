26761a337b9f4e05c4b942e78c10e096ac360f0f
Theo Iyonor <symplytheo@gmail.com>
2021-11-21 19:00:54 +0100

    add user menu and mutation to setUser

--- store/user.js
+++ store/user.js
@@ -1,10 +1,22 @@
 export const state = () => ({
   profile: null,
   authenticated: false,
-  userMenu: [],
+  userMenu: [
+    { title: 'Profile', href: '', icon: '' },
+    { title: 'Bump up', href: '', icon: '' },
+    { title: 'Refer a Friend', href: '', icon: '' },
+    { title: 'Send Feedback', href: '', icon: '' },
+  ],
 })
 
 export const getters = {
   profile: (state) => state.profile,
   authenticated: (state) => state.authenticated,
+}
+
+export const mutations = {
+  setUser(state, user) {
+    state.profile = user || null
+    state.authenticated = !!user
+  },
 }
